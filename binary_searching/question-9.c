bba2e7f93bbd57c185b336a6a7977d5c3cc24ef0
Debjit Bhattacharya <141891710+Debjit28@users.noreply.github.com>
2024-11-24 19:02:44 +0530

    Create question-9.c

---
+++ binary_searching/question-9.c
@@ -0,0 +1,21 @@
+int searchInNearlySortedArray(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (mid > low && arr[mid - 1] == target) {
+            return mid - 1;
+        }
+        if (mid < high && arr[mid + 1] == target) {
+            return mid + 1;
+        }
+        if (arr[mid] > target) {
+            high = mid - 2;
+        } else {
+            low = mid + 2;
+        }
+    }
+    return -1;
+}
