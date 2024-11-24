26e1cf123c26338ada5a19fe6b02224b4d50565f
Debjit Bhattacharya <141891710+Debjit28@users.noreply.github.com>
2024-11-24 18:44:05 +0530

    Create mcm.c

---
+++ experiment-3/mcm.c
@@ -0,0 +1,33 @@
+//matrix chain multiplication problem
+#include <stdio.h>
+#include <limits.h>
+
+void matrixChainOrder(int p[], int n) {
+    int m[n][n];
+    int s[n][n];
+
+    for (int i = 1; i < n; i++) m[i][i] = 0;
+
+    for (int l = 2; l < n; l++) {
+        for (int i = 1; i < n - l + 1; i++) {
+            int j = i + l - 1;
+            m[i][j] = INT_MAX;
+            for (int k = i; k < j; k++) {
+                int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                if (q < m[i][j]) {
+                    m[i][j] = q;
+                    s[i][j] = k;
+                }
+            }
+        }
+    }
+
+    printf("Minimum number of multiplications: %d\n", m[1][n - 1]);
+}
+
+int main() {
+    int arr[] = {40, 20, 30, 10, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    matrixChainOrder(arr, n);
+    return 0;
+}
